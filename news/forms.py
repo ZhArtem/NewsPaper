c0a1539ef34eeff2138695e68f8b499860d8eb4e
Artem Zhoglo <zhogloaa@yandex.ru>
2023-07-27 17:26:09 +0300

    Minor changes

--- news/forms.py
+++ news/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm, Select
 from .models import Post
 
@@ -6,4 +7,20 @@ class AddPostForm(ModelForm):
     class Meta:
         model = Post
         exclude = ['rating']
-
+        widgets = {
+            'author': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'categoryType': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'postCategory': forms.SelectMultiple(attrs={
+                'class': 'form-control'
+            }),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'text': forms.Textarea(attrs={
+                'class': 'form-control'
+            }),
+        }
